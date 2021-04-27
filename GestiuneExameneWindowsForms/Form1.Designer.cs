59855e586790d29c29cb80de1f136158a9fb9865
Dana Gheorghita <dana.gheorghita@napa.fi>
2021-04-27 03:54:10 +0300

    Add UI and logic to Form1 for choosing which Faculty is the data processed

--- GestiuneExameneWindowsForms/Form1.Designer.cs
+++ GestiuneExameneWindowsForms/Form1.Designer.cs
@@ -29,18 +29,22 @@ namespace GestiuneExameneWindowsForms
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.buttonAdaugaDate = new System.Windows.Forms.Button();
             this.buttonProgramareExamene = new System.Windows.Forms.Button();
             this.buttonStatistici = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
+            this.comboBoxListaFacultati = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelFacultateCurentSelectata = new System.Windows.Forms.Label();
+            this.label_IDFacultateCurentSelectata = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonAdaugaDate
             // 
-            this.buttonAdaugaDate.Location = new System.Drawing.Point(42, 102);
+            this.buttonAdaugaDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdaugaDate.Location = new System.Drawing.Point(58, 207);
             this.buttonAdaugaDate.Name = "buttonAdaugaDate";
-            this.buttonAdaugaDate.Size = new System.Drawing.Size(126, 45);
+            this.buttonAdaugaDate.Size = new System.Drawing.Size(178, 45);
             this.buttonAdaugaDate.TabIndex = 0;
             this.buttonAdaugaDate.Text = "Adauga Date";
             this.buttonAdaugaDate.UseVisualStyleBackColor = true;
@@ -48,9 +52,10 @@ namespace GestiuneExameneWindowsForms
             // 
             // buttonProgramareExamene
             // 
-            this.buttonProgramareExamene.Location = new System.Drawing.Point(174, 102);
+            this.buttonProgramareExamene.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonProgramareExamene.Location = new System.Drawing.Point(242, 207);
             this.buttonProgramareExamene.Name = "buttonProgramareExamene";
-            this.buttonProgramareExamene.Size = new System.Drawing.Size(128, 45);
+            this.buttonProgramareExamene.Size = new System.Drawing.Size(232, 45);
             this.buttonProgramareExamene.TabIndex = 1;
             this.buttonProgramareExamene.Text = "Programare Examene";
             this.buttonProgramareExamene.UseVisualStyleBackColor = true;
@@ -58,9 +63,10 @@ namespace GestiuneExameneWindowsForms
             // 
             // buttonStatistici
             // 
-            this.buttonStatistici.Location = new System.Drawing.Point(309, 102);
+            this.buttonStatistici.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStatistici.Location = new System.Drawing.Point(480, 207);
             this.buttonStatistici.Name = "buttonStatistici";
-            this.buttonStatistici.Size = new System.Drawing.Size(128, 45);
+            this.buttonStatistici.Size = new System.Drawing.Size(206, 45);
             this.buttonStatistici.TabIndex = 2;
             this.buttonStatistici.Text = "Statistici si rapoarte";
             this.buttonStatistici.UseVisualStyleBackColor = true;
@@ -68,20 +74,67 @@ namespace GestiuneExameneWindowsForms
             // 
             // buttonExit
             // 
-            this.buttonExit.Location = new System.Drawing.Point(392, 224);
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(627, 439);
             this.buttonExit.Name = "buttonExit";
-            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.Size = new System.Drawing.Size(101, 43);
             this.buttonExit.TabIndex = 3;
             this.buttonExit.Text = "Iesire";
             this.buttonExit.UseVisualStyleBackColor = true;
             this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
             // 
+            // comboBoxListaFacultati
+            // 
+            this.comboBoxListaFacultati.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxListaFacultati.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxListaFacultati.FormattingEnabled = true;
+            this.comboBoxListaFacultati.Location = new System.Drawing.Point(58, 127);
+            this.comboBoxListaFacultati.Name = "comboBoxListaFacultati";
+            this.comboBoxListaFacultati.Size = new System.Drawing.Size(628, 32);
+            this.comboBoxListaFacultati.TabIndex = 4;
+            this.comboBoxListaFacultati.SelectedIndexChanged += new System.EventHandler(this.facultateForm1_selectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(281, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Alegeti facultatea:";
+            // 
+            // labelFacultateCurentSelectata
+            // 
+            this.labelFacultateCurentSelectata.AutoSize = true;
+            this.labelFacultateCurentSelectata.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFacultateCurentSelectata.Location = new System.Drawing.Point(54, 368);
+            this.labelFacultateCurentSelectata.Name = "labelFacultateCurentSelectata";
+            this.labelFacultateCurentSelectata.Size = new System.Drawing.Size(24, 24);
+            this.labelFacultateCurentSelectata.TabIndex = 6;
+            this.labelFacultateCurentSelectata.Text = "X";
+            this.labelFacultateCurentSelectata.Visible = false;
+            // 
+            // label_IDFacultateCurentSelectata
+            // 
+            this.label_IDFacultateCurentSelectata.AutoSize = true;
+            this.label_IDFacultateCurentSelectata.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_IDFacultateCurentSelectata.Location = new System.Drawing.Point(54, 413);
+            this.label_IDFacultateCurentSelectata.Name = "label_IDFacultateCurentSelectata";
+            this.label_IDFacultateCurentSelectata.Size = new System.Drawing.Size(24, 24);
+            this.label_IDFacultateCurentSelectata.TabIndex = 7;
+            this.label_IDFacultateCurentSelectata.Text = "X";
+            this.label_IDFacultateCurentSelectata.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(479, 259);
+            this.ClientSize = new System.Drawing.Size(740, 494);
+            this.Controls.Add(this.label_IDFacultateCurentSelectata);
+            this.Controls.Add(this.labelFacultateCurentSelectata);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxListaFacultati);
             this.Controls.Add(this.buttonExit);
             this.Controls.Add(this.buttonStatistici);
             this.Controls.Add(this.buttonProgramareExamene);
@@ -89,7 +142,9 @@ namespace GestiuneExameneWindowsForms
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Gestiune Examene";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -99,6 +154,10 @@ namespace GestiuneExameneWindowsForms
         private System.Windows.Forms.Button buttonProgramareExamene;
         private System.Windows.Forms.Button buttonStatistici;
         private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.ComboBox comboBoxListaFacultati;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelFacultateCurentSelectata;
+        private System.Windows.Forms.Label label_IDFacultateCurentSelectata;
     }
 }
 
--- GestiuneExameneWindowsForms/Form1.cs
+++ GestiuneExameneWindowsForms/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,111 @@ namespace GestiuneExameneWindowsForms
         public Form1()
         {
             InitializeComponent();
+            conectare();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conectare();
+            completareDataSet();
+            adaugaFacultateToDropDownList();
+            setFacultateSelectata();
+            showCurrentFaculty();
+        }
+
+        #region declarare variabile
+        SqlConnection con;
+        SqlDataAdapter da;
+        DataSet ds = new DataSet();
+        static string idFacultateSelectata;
+        static string denumireFacultateSelectata;
+        #endregion
+
+        #region conectare
+        void conectare()
+        {
+            con = new SqlConnection();
+            con.ConnectionString = @"Data Source=.;Initial Catalog=GestiuneExamene;Integrated Security=True"; //connection string pentru BD de pe SQL Server
+        }
+        #endregion
+
+        #region DataSet
+        void completareDataSet()
+        {
+            //Open connection
+            con.Open();
+
+            //Query strings
+            string selectFacultate = "SELECT * FROM Facultate";
+
+            //DataAdapter+DataSet
+            da = new SqlDataAdapter(selectFacultate, con);
+            da.Fill(ds, "FACULTATE");
+
+            //Close connection
+            con.Close();
+
+        }
+        #endregion
+
+        #region PopulateDropDownLists
+        void adaugaFacultateToDropDownList()
+        {
+            comboBoxListaFacultati.Items.Clear();
+
+            foreach (DataRow dr in ds.Tables["FACULTATE"].Rows)
+                comboBoxListaFacultati.Items.Add(dr.ItemArray.GetValue(1).ToString());
+
+            if (comboBoxListaFacultati.Items.Count > 0)
+                comboBoxListaFacultati.SelectedIndex = 0;
+        }
+        #endregion
+
+        #region SET_FacultateSelectata
+        public void setFacultateSelectata()
+        {
+            denumireFacultateSelectata = comboBoxListaFacultati.SelectedItem.ToString();
+
+            foreach (DataRow dr in ds.Tables["FACULTATE"].Rows)
+                if (dr.ItemArray.GetValue(1).ToString() == comboBoxListaFacultati.SelectedItem.ToString())
+                    idFacultateSelectata = dr.ItemArray.GetValue(0).ToString();
+        }
+
+        bool activareEvenimentDropDownListFacultate = false;
+
+        private void facultateForm1_selectedIndexChanged(object sender, EventArgs e)
+        {
+            //false => se modifica interactiv lista in functie de selectie
+            if (!activareEvenimentDropDownListFacultate)
+            {
+                setFacultateSelectata();
+                showCurrentFaculty();
+            }
+
+        }
+        #endregion
+
+        #region selectieCurentaFacultate_GUI
+        public void showCurrentFaculty()
+        {
+            if (!String.IsNullOrEmpty(denumireFacultateSelectata))
+            {
+                labelFacultateCurentSelectata.Visible = true;
+                labelFacultateCurentSelectata.Text = "Selectia curenta: " + denumireFacultateSelectata;
+
+                label_IDFacultateCurentSelectata.Visible = true;
+                label_IDFacultateCurentSelectata.Text = "COD: " + idFacultateSelectata;
+            }
+            else
+            {
+                labelFacultateCurentSelectata.Visible = true;
+                labelFacultateCurentSelectata.Text = "Eroare selectie";
+
+                label_IDFacultateCurentSelectata.Visible = true;
+                label_IDFacultateCurentSelectata.Text = "EROARE";
+            }
+        }
+        #endregion
 
         AddDataForm addDataForm = new AddDataForm();
         private void buttonAdaugaDate_Click(object sender, EventArgs e)
